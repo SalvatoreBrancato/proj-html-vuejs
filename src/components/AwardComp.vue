612350d4b93713a8141d21586884ebcadb2f2c1d
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-05-12 17:07:00 +0200

    AwardComp

---
+++ src/components/AwardComp.vue
@@ -0,0 +1,46 @@
+<script>
+
+
+export default{
+  name: "AwardComp",
+  components:{
+    
+}
+}
+</script>
+
+<template>
+    <div id="container-award" class="d-flex justify-content-center align-items-center flex-column">
+        <h3>AWARD WINNING</h3>
+        <h2>Pro Barbers</h2>
+        <button>MEET NOW YOUR BARBER</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    #container-award{
+        background-image: url(../../public/img/avadabarbers-about-introbackground.jpg);
+        height: 50vh;
+        h2{
+            color: rgba(255, 255, 255, 0.6);
+            font-size: 90px;
+            font-family: 'Abril Fatface', cursive;
+        }
+        h3{
+            color:#ae8652;
+            font-size: 20px;
+            letter-spacing: 15px;
+        }
+        button{
+            border: 2px solid #ae8652;
+            background-color: rgba(0, 0, 0, 0.0);
+            color: #ae8652;  
+            padding: 15px;
+            font-weight: bold;
+            &:hover{
+                background-color: #ae8652;
+                color: black;
+            }        
+        }  
+    }
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,16 +1,19 @@
 <script>
+import AwardComp from './AwardComp.vue';
 import NavComp from './NavComp.vue';
 
 export default{
   name: "MainComp",
   components:{
-    NavComp
+    NavComp,
+    AwardComp
 }
 }
 </script>
 
 <template>
     <NavComp/>
+    <AwardComp/>
 </template>
 
 <style lang="scss" scoped>
